4dd085b0370174ff26e10cf135b4c64205f0ef84
sorgazb <sorgazb@gmail.com>
2025-01-11 23:57:26 +0100

    Crea proyecto Angular y añade librerias de componentes Angular

---
+++ TFC_Frontend/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { es_ES } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import es from '@angular/common/locales/es';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+registerLocaleData(es);
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [
+    { provide: NZ_I18N, useValue: es_ES }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
