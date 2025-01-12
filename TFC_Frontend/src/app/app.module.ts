53a097ac7249676b8e2877734261e30bc30fc3bf
sorgazb <sorgazb@gmail.com>
2025-01-12 14:20:47 +0100

    Incorpora taildwin css y comienza a hacer el formulario de login

--- TFC_Frontend/src/app/app.module.ts
+++ TFC_Frontend/src/app/app.module.ts
@@ -8,12 +8,14 @@ import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { es_ES } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import es from '@angular/common/locales/es';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { InicioComponent } from './inicio/inicio.component';
 import { HeaderComponent } from './header/header.component';
 import {MatIconModule} from '@angular/material/icon';
 import { InicioSesionComponent } from './inicio-sesion/inicio-sesion.component'
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input'
 
 registerLocaleData(es);
 
@@ -30,7 +32,10 @@ registerLocaleData(es);
     BrowserAnimationsModule,
     FormsModule,
     HttpClientModule,
-    MatIconModule
+    MatIconModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [
     { provide: NZ_I18N, useValue: es_ES }
--- TFC_Frontend/src/app/inicio-sesion/inicio-sesion.component.ts
+++ TFC_Frontend/src/app/inicio-sesion/inicio-sesion.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-inicio-sesion',
   templateUrl: './inicio-sesion.component.html',
   styleUrls: ['./inicio-sesion.component.css']
 })
-export class InicioSesionComponent {
+export class InicioSesionComponent implements OnInit{
+  formLogin !: FormGroup
 
+  constructor(){}
+
+  ngOnInit(): void {
+    this.formLogin = new FormGroup({
+      email : new FormControl('',[Validators.required]),
+      password : new FormControl('',[Validators.required])
+    })
+  }
+
+  public controlarErrores(nombreControl : string, nombreError : string){
+    return this.formLogin.controls[nombreControl].hasError(nombreError)
+  }
 }
